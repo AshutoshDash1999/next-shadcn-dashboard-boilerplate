5500c18506eae46cdf9367e711938503f20da463
Ashutosh Dash <dashashutosh1999@gmail.com>
2026-05-24 14:02:55 +0530

    feat: add Sheet and Sidebar components for improved UI layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,5 @@
+import Sidebar03 from "@/components/sidebar-03";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <Sidebar03>{children}</Sidebar03>;
+}
